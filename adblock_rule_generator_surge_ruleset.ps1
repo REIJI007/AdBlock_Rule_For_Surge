fc524689ad34bb6cf57155f325b061def6b3f5f0
REIJI007 <miyajimareji007@gmail.com>
2024-08-12 15:46:41 +0800

    Update adblock_rule_generator_surge_ruleset.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_surge_ruleset.ps1
+++ adblock_rule_generator_surge_ruleset.ps1
@@ -97,7 +97,7 @@ $($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
-$outputPath = "$PSScriptRoot/adblock_reject_surge_ruleset.txt"
+$outputPath = "$PSScriptRoot/adblock_reject_surge_ruleset.list"
 $textContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数

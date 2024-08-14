70b393a8d7a15e48852e01fd474218052eaf8d13
REIJI007 <miyajimareji007@gmail.com>
2024-08-14 23:48:10 +0800

    Update adblock_rule_generator_surge_ruleset.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_surge_ruleset.ps1
+++ adblock_rule_generator_surge_ruleset.ps1
@@ -4,7 +4,7 @@
 
 # 定义广告过滤器URL列表
 $urlList = @(
- "https://anti-ad.net/adguard.txt",
+    "https://anti-ad.net/adguard.txt",
     "https://anti-ad.net/easylist.txt",
     "https://easylist-downloads.adblockplus.org/easylist.txt",
     "https://easylist-downloads.adblockplus.org/easylistchina.txt",
@@ -58,7 +58,26 @@ foreach ($url in $urlList) {
         $lines = $content -split "`n"
 
         foreach ($line in $lines) {
-            if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$' -or $line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$' -or $line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
+            # 匹配 Adblock/Easylist 格式的规则
+            if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
+                $domain = $Matches[1]
+                if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
+                    $uniqueRules.Add($domain) | Out-Null
+                } else {
+                    Add-Content -Path $logFilePath -Value "无效或超长域名: $domain"
+                }
+            }
+            # 匹配 Hosts 文件格式的规则
+            elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
+                $domain = $Matches[2]
+                if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
+                    $uniqueRules.Add($domain) | Out-Null
+                } else {
+                    Add-Content -Path $logFilePath -Value "无效或超长域名: $domain"
+                }
+            }
+            # 匹配 Dnsmasq/AdGuard 格式的规则
+            elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
                 $domain = $Matches[1]
                 if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
                     $uniqueRules.Add($domain) | Out-Null

3f72fc674d38cedf4ac0bbc3dae6e0cf0978dad6
REIJI007 <miyajimareji007@gmail.com>
2024-08-18 02:13:58 +0800

    Update adblock_rule_generator_surge_ruleset.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_surge_ruleset.ps1
+++ adblock_rule_generator_surge_ruleset.ps1
@@ -61,29 +61,22 @@ foreach ($url in $urlList) {
             # 匹配 Adblock/Easylist 格式的规则
             if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
                 $domain = $Matches[1]
-                if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
-                    $uniqueRules.Add($domain) | Out-Null
-                } else {
-                    Add-Content -Path $logFilePath -Value "无效或超长域名: $domain"
-                }
+                $uniqueRules.Add($domain) | Out-Null
             }
             # 匹配 Hosts 文件格式的规则
             elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
                 $domain = $Matches[2]
-                if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
-                    $uniqueRules.Add($domain) | Out-Null
-                } else {
-                    Add-Content -Path $logFilePath -Value "无效或超长域名: $domain"
-                }
+                $uniqueRules.Add($domain) | Out-Null
             }
             # 匹配 Dnsmasq/AdGuard 格式的规则
             elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
                 $domain = $Matches[1]
-                if ($domain -match '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$' -and $domain.Length -le 50) {
-                    $uniqueRules.Add($domain) | Out-Null
-                } else {
-                    Add-Content -Path $logFilePath -Value "无效或超长域名: $domain"
-                }
+                $uniqueRules.Add($domain) | Out-Null
+            }
+            # 匹配通配符匹配格式的规则
+            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
+                $domain = $Matches[1]
+                $uniqueRules.Add($domain) | Out-Null
             }
         }
     }
@@ -92,6 +85,7 @@ foreach ($url in $urlList) {
         Add-Content -Path $logFilePath -Value "处理 $url 时出错: $_"
     }
 }
+
 
 # 对规则进行排序并添加DOMAIN,前缀
 $formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"DOMAIN,$_"}

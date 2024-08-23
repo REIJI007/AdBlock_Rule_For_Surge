a764b4090e9c6b3809c206fbc670d735bd6afbae
REIJI007 <miyajimareji007@gmail.com>
2024-08-23 11:26:13 +0800

    Update adblock_rule_generator_surge_ruleset.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_surge_ruleset.ps1
+++ adblock_rule_generator_surge_ruleset.ps1
@@ -91,7 +91,7 @@ foreach ($url in $urlList) {
 
 
 # 对规则进行排序并添加DOMAIN,前缀
-$formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"DOMAIN,$_"}
+$formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"DOMAIN-SUFFIX,$_"}
 
 # 统计生成的规则条目数量
 $ruleCount = $uniqueRules.Count

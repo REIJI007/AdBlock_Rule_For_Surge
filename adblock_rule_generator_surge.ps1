d0e10713d9b7945741de7fb84bfd1df489279f50
REIJI007 <miyajimareji007@gmail.com>
2024-09-13 12:22:38 +0800

    Update adblock_rule_generator_surge.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_surge.ps1
+++ adblock_rule_generator_surge.ps1
@@ -136,7 +136,8 @@ $urlList = @(
     "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/adblock/adblock.txt",
     "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
     "https://raw.githubusercontent.com/damengzhu/banad/main/dnslist.txt",
-    "https://hblock.molinero.dev/hosts_adblock.txt"
+    "https://hblock.molinero.dev/hosts_adblock.txt",
+    "https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/adblock.txt"
 )
 
 # 日志文件路径
